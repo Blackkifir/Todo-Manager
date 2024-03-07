7a9aeee906342e0bab0cc7a3ff2c1687c9845015
Nikiforuk <goldlamborgini@gmail.com>
2024-03-07 11:03:04 +0200

    redux: add store and todosSlice

--- src/components/Task/task.tsx
+++ src/components/Task/task.tsx
@@ -4,6 +4,7 @@ import styles from './task.module.scss';
 const Task: React.FC = () => {
   return (
     <div className={styles.task}>
+      <input type='checkbox' className={styles.task_checkBox} />
       <p className={styles.task_text}>task</p>
       <button className={styles.task_btnDelete}>
         <MdDelete className={styles.task_btnDelete_icon} />
